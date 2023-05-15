05ea0a4741238f06ede5cabf635181d98a4205df
Tahina <rhhtahina@gmail.com>
2023-05-15 20:39:11 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autocomplete textbox using jQuery, PHP and MySQL</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <style>
+        ul {
+            background-color: #eee;
+            cursor: pointer;
+        }
+
+        li {
+            padding: 12px;
+        }
+    </style>
+</head>
+
+<body>
+    <br><br>
+    <div class="container" style="width:500px">
+        <h3 align="center">Autocomplete textbox using jQuery, PHP and MySQL</h3>
+        <label>Enter Country Name</label>
+        <input type="text" name="country" id="country" class="form-control" placeholder="Enter Country Name">
+        <div id="countryList"></div>
+    </div>
+</body>
+
+</html>
+
+<script>
+    $(document).ready(function() {
+        $("#country").keyup(function(e) {
+            var query = $(this).val()
+            if (query != '') {
+                $.ajax({
+                    method: "post",
+                    url: "search.php",
+                    data: {
+                        query: query
+                    },
+                    success: function(data) {
+                        $('#countryList').fadeIn()
+                        $('#countryList').html(data)
+                    }
+                });
+            } else {
+                $('#countryList').fadeOut()
+                $('#countryList').html("")
+            }
+        });
+        $(document).on('click', 'li', function() {
+            $('#country').val($(this).text())
+            $('#countryList').fadeOut()
+        })
+    });
+</script>
